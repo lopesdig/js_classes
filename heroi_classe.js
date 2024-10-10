559bfe560200fdf5024e6402e54eeaa93fab25a8
W3café <60747896+lopesdig@users.noreply.github.com>
2024-10-09 22:42:19 -0300

    Ataque_do_heroi

---
+++ heroi_classe.js
@@ -0,0 +1,24 @@
+let nome = `Felipe`;
+let idade = 30 ;
+
+class heroi{
+	constructor(tipo, ataque){
+    this.tipo = tipo;
+    this.ataque = ataque;
+  }
+  
+  escrever(){
+	console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+  }
+     
+}
+
+let guerreiro = new heroi(`guerreiro` , `espada`);
+let mago = new heroi(`mago`,`magia`);
+let monge = new heroi(`monge`,`artes marciais`);
+let ninja = new heroi(`nijna`,`shuriken`);
+
+guerreiro.escrever()
+mago.escrever()
+monge.escrever()
+ninja.escrever()
